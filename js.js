0190142c30bcaa3fbdf3ec93c6c20df23495eeb7
Gaston Maina <gaston.maina.sj@gmail.com>
2023-12-04 06:54:35 -0300

    Reservas

--- js.js
+++ js.js
@@ -1,7 +1,20 @@
 const btnEnviar = document.getElementById("btnEnviar")
 const divLoading = document.getElementById("loadingIcon")
+const formularioReservas = document.getElementById("formularioReservas")
 
-btnEnviar.addEventListener('click', ()=>{
-    divLoading.classList.add("popup")
-    divLoading.classList.remove("oculto")    
-})
+// btnEnviar.addEventListener('click', ()=>{
+//     divLoading.classList.add("popup")
+//     divLoading.classList.remove("oculto")    
+// })
+
+formularioReservas.addEventListener("click", handleFormularioReserva)
+
+
+function handleFormularioReserva(e){
+    
+    if(e.target.name == "btnDisponibilidad"){
+        e.preventDefault();
+        console.log(formularioReservas.fechaInicio.value, formularioReservas.fechaSalida.value)
+    }
+    
+}
